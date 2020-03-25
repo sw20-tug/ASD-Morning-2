72ba45e83d7f68421f48e4d0949646a3b42aee3c
Martin Mayr <me@mrtn.it>
2020-03-25 08:12:25 +0100

    reset and init new project

---
+++ server/src/test/java/at/tugraz/asd/LANG/LangApplicationTests.java
@@ -0,0 +1,13 @@
+package at.tugraz.asd.LANG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
