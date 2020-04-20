8e9f51beccb983209b6709409023ce99bca35643
RaeNon <44230124+RaeNon@users.noreply.github.com>
2020-04-20 09:02:36 +0200

    Class rewrite, Navbar, Buttons

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,9 +2,7 @@ import React from 'react'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../components/base.css';
-import '../components/Navbar.css';
-import '../components/SideDrawerButton.css';
-import '../components/SideDrawer.css';
+import '../components/Navigationbar.css';
 
 export default function MyApp({Component, pageProps}){
   return <Component {...pageProps} />
