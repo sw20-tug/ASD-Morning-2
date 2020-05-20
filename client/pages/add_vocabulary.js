7434c7c68a8d60dbfb9da773908978bd81ff2978
Lukas Mathä <mathae@student.tugraz.at>
2020-05-20 11:00:45 +0200

    [LANG-014] Reworked add_vocabulary - missing topics

--- client/pages/add_vocabulary.js
+++ client/pages/add_vocabulary.js
@@ -1,4 +1,4 @@
-import { Modal, Button, Row, Col, Form, Dropdown, InputGroup } from 'react-bootstrap';
+/*import { Modal, Button, Row, Col, Form, Dropdown, InputGroup } from 'react-bootstrap';
 import fetch from 'isomorphic-unfetch';
 import { Container } from "next/app";
 import ModalHeader from 'react-bootstrap/ModalHeader';
@@ -104,6 +104,143 @@ class AddVocabulary extends React.Component {
                     <Col><Button style={{ width: "200px" }} type="button" onClick={this.sendData}>Save new Vocabulary</Button></Col>
                     <Col></Col>
                 </Row>
+            </main>
+        );
+    }
+}
+
+export default AddVocabulary;*/
+
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Rating from 'react-rating';
+import { Form, Dropdown } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Link from 'next/link';
+
+
+class AddVocabulary extends React.Component {
+    constructor() {
+        super();
+        this.sendData = this.sendData.bind(this);
+        this.state = {
+            vocabulary: '',
+            translationsDE: '',
+            translationsEN: '',
+            translationsFR: '',
+        };
+    };
+
+    async sendData() {
+        var body = this.prepareData();
+        if(body == -1)
+            return;
+        console.log(body)
+        var result = await fetch('http://localhost:8080/api/vocabulary', {
+            method: 'POST',
+            headers: new Headers({'content-type': 'application/json'}),
+            body: body
+        });
+        if(result.status == 200){
+            alert("OK saved!")
+        }
+        else{
+            alert("An error occured!")
+        }
+    }
+    prepareData(){
+        if(this.state.translationsDE == '' || this.state.translationsEN=='' || this.state.translationsFR=='')
+        {
+            alert('Pleas provied the necessary input!')
+            return -1;
+        }
+        else{
+            return JSON.stringify({
+                vocabulary: this.state.vocabulary,
+                translations: {
+                    'DE' : this.state.translationsDE,
+                    'EN' : this.state.translationsEN,
+                    'FR' : this.state.translationsFR
+                }
+            })
+        }
+        
+    }
+    handleChange_Vocabulary = (e) => {
+        if(e.target.value == 'DE')
+        {
+            this.setState({ vocabulary: this.state.translationsDE });
+        }
+        if(e.target.value == 'FR')
+        {
+            this.setState({ vocabulary: this.state.translationsFR });
+        }
+        if(e.target.value == 'EN')
+        {
+            this.setState({ vocabulary: this.state.translationsEN });
+        } 
+    }
+    handleChange_DE = (e) => {
+        this.setState({translationsDE: e.target.value});
+    }
+    handleChange_EN = (e) => {
+        this.setState({translationsEN: e.target.value});
+    }
+    handleChange_FR = (e) => {
+        this.setState({translationsFR: e.target.value});
+    }
+
+ 
+    render() {
+        return(
+            <main className="edit_main">
+                <Form.Group className="edit_container">
+                    <h2>
+                        Add Vocabulary
+                    </h2>
+                    <Form.Group style={{ width: 220 }} controlId="select_language" onChange={this.handleChange_Vocabulary}>
+                        <select variant="primary"  name="translation_language">
+                            <option value="sel_lang">Select language of key vocabulary</option>
+                            <option value="DE">German</option>
+                            <option value="EN">English</option>
+                            <option value="FR">French</option>
+                        </select>
+                    </Form.Group>
+
+                    <Form.Group className="edit_input_wrapper">
+                        <InputGroup size="sm" className="mb-3">
+                            <InputGroup.Prepend>
+                                <InputGroup.Text id="inputGroup-sizing-sm">DE</InputGroup.Text>
+                            </InputGroup.Prepend>
+                            <FormControl type="text" name="DE" onChange={ this.handleChange_DE }aria-label="Small" aria-describedby="inputGroup-sizing-sm" />
+                        </InputGroup>
+                        <InputGroup size="sm" className="mb-3">
+                            <InputGroup.Prepend>
+                                <InputGroup.Text id="inputGroup-sizing-sm">EN</InputGroup.Text>
+                            </InputGroup.Prepend>
+                            <FormControl type="text" name="EN" onChange={ this.handleChange_EN } aria-label="Small" aria-describedby="inputGroup-sizing-sm" />
+                        </InputGroup>
+                        <InputGroup  size="sm" className="mb-3">
+                            <InputGroup.Prepend>
+                                <InputGroup.Text id="inputGroup-sizing-sm">FR</InputGroup.Text>
+                            </InputGroup.Prepend>
+                            <FormControl type="text" name="FR" onChange={ this.handleChange_FR } aria-label="Small" aria-describedby="inputGroup-sizing-sm" />
+                        </InputGroup>
+
+                        <InputGroup  size="sm" className="mb-3">
+                           <Rating onChange={this.rate} initialRating={this.state.initialRating} fractions="1"/>
+                        </InputGroup>
+                    
+                    </Form.Group>
+                    <hr/>
+                    <div className="edit_button_wrap">
+                        <Link href="/vocabulary" replace>
+                            <Button variant="primary">Return</Button>
+                        </Link>
+                        <Button type="button" onClick={ this.sendData } variant="primary" disabled = {!this.state.translationsDE || 
+                            !this.state.translationsEN ||!this.state.translationsFR || !this.state.vocabulary}>Add new Vocabulary</Button>
+                    </div>
+                </Form.Group>
             </main>
         );
     }
